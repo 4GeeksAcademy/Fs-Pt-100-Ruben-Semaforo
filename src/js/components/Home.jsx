4a373fb3350bd7ff52a230d2dc00099a89500114
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-04-07 11:10:13 +0000

    semaforo completado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [colors, setColors] = useState("red")
+	const [purple, setPurple] = useState(false)
+
+	const changeColors = () => {
+		if (purple) {
+			if (colors === "redLigth") {
+			setColors ("yellowLigth")
+			} 
+			if (colors === "yellowLigth") {
+			setColors ("greenLigth")
+			}
+			if (colors === "greenLigth") {
+			setColors ("purpleLigth")
+			}
+			if (colors === "purpleLigth") {
+			setColors ("redLigth")
+			}
+		} else {
+			if (colors === "redLigth") {
+			setColors ("yellowLigth")
+			} 
+			if (colors === "yellowLigth") {
+			setColors ("greenLigth")
+			}
+			if (colors === "greenLigth") {
+			setColors ("redLigth")
+			}
+		}
+		
+	}
+	const randomLigth = () => {
+		const colores = ['redLigth', 'greenLigth', 'yelowLigth', 'purpleLigth']
+		const random = colores[Math.floor(Math.random()*colores.length)]
+		setColors(random)
+	}
+
+	const colorPurple = () => {
+		setPurple(!purple)
+	}
+
+	const handleClick = (val) => {
+		setColors(val)
+	}
+
+
+	return (
+		<div className="back conteiner-fluid">
+			<div className="paloSemaforo">
+			</div>
+			<div className="semaforo container">
+				<div className={`redLigth ${colors === "redLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('redLigth')}></div>
+				<div className={`yellowLigth ${colors === "yellowLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('yellowLigth')}></div>
+				<div className={`greenLigth ${colors === "greenLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('greenLigth')}></div>
+				{purple && (
+				<div className={`purpleLigth ${colors === "purpleLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('purpleLigth')}></div>
+				)}
+			</div>
+
+			<div className="buttonColors">
+				<button className="btn btn-primary" onClick={changeColors}>Change Colors</button>
+				<button className="btn btn-primary" onClick={randomLigth}>Random Ligth</button>
+			</div>
+			<div>
+				<button className="btn btn-primary" onClick={colorPurple}>Purple Button</button>
+			</div>
 		</div>
+		
 	);
 };
 
